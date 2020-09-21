1225be0a099f31fe30ce073f9dfd019b81696661
echchorfisalahedine <echchorfi2014@gmail.com>
2020-09-21 18:03:41 +0100

    Updates

---
+++ precache-manifest.eb38c5ad152a0d3a31efa9e1c86c5d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4886d63b389632bb542d2237301c49",
+    "url": "/recipes2.0/index.html"
+  },
+  {
+    "revision": "31e526db1061f8003015",
+    "url": "/recipes2.0/static/css/main.06c1eeba.chunk.css"
+  },
+  {
+    "revision": "25dba4d3b2f7e95aff02",
+    "url": "/recipes2.0/static/js/2.d3754bda.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/recipes2.0/static/js/2.d3754bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e526db1061f8003015",
+    "url": "/recipes2.0/static/js/main.b842e8be.chunk.js"
+  },
+  {
+    "revision": "04273429d1e31c73eb93",
+    "url": "/recipes2.0/static/js/runtime-main.5d93415e.js"
+  }
+]);
